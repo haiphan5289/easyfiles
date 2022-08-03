43126d996a607624b7bde1e54d29e3c7810ab5ff
haiphan5289 <haiphan5289@gmail.com>
2022-08-03 21:26:07 +0700

    init

---
+++ EasyFiles/EasyFiles/EasyFiles.h
@@ -0,0 +1,18 @@
+//
+//  EasyFiles.h
+//  EasyFiles
+//
+//  Created by haiphan on 03/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyFiles.
+FOUNDATION_EXPORT double EasyFilesVersionNumber;
+
+//! Project version string for EasyFiles.
+FOUNDATION_EXPORT const unsigned char EasyFilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyFiles/PublicHeader.h>
+
+
---
+++ EasyFiles/EasyFiles/EasyFiles.swift
@@ -0,0 +1,16 @@
+//
+//  EasyFiles.swift
+//  EasyFiles
+//
+//  Created by haiphan on 03/08/2022.
+//
+
+import Foundation
+
+public struct EasyFile {
+    
+    public static func SayHello() {
+        print("=== Hello ====")
+    }
+    
+}
